6fa0086e7726c83a20e76062a3b5bf5b4a269806
Miclain Keffeler <user@mkkeffeler.local>
2017-03-25 18:58:34 -0500

    Still Geocoding locations, obtained crime data from web portal.

--- geocode.py
+++ geocode.py
@@ -5,10 +5,9 @@ from pprint import pprint
 geolocator = Nominatim()
 
 
-with open('run_results.json') as infile:
-    list_of_apartments = json.load(infile)
-    for line in list_of_apartments:
-        print (line)
-        #pprint (list_of_apartments)
-#    location = geolocator.geocode("")
-        #print((location.latitude, location.longitude))
+with open('run_results.txt') as infile:
+    for line in infile:
+        location = geolocator.geocode(line)
+        with open('lat_long_apartments.json','w') as list_of_apartments:
+
+            list_of_apartments.write(location)
