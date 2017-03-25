7c88e0e17ca3fc12ba07ac45e39b3aed0d00e522
Miclain Keffeler <user@mkkeffeler.local>
2017-03-25 17:53:33 -0500

    Pivoting

--- geocode.py
+++ geocode.py
@@ -1,11 +1,14 @@
 from geopy.geocoders import Nominatim
 import json
+from pprint import pprint
+
 geolocator = Nominatim()
 
 
 with open('run_results.json') as infile:
-        list_of_apartments = json.load(infile)
-        for name in list_of_apartments:
-            print name
-            location = geolocator.geocode("")
-            print((location.latitude, location.longitude))
+    list_of_apartments = json.load(infile)
+    for line in list_of_apartments:
+        print (line)
+        #pprint (list_of_apartments)
+#    location = geolocator.geocode("")
+        #print((location.latitude, location.longitude))
