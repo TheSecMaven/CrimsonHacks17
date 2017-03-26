f0b862550a23af4862d004d783119041dab5c83d
Miclain Keffeler <user@mkkeffeler.local>
2017-03-26 10:14:12 -0500

    Associated building permits within .15 mile radius with a respective apartment complex.

--- building_permit_association.py
+++ building_permit_association.py
@@ -2,32 +2,39 @@ from geopy.distance import vincenty
 from geopy.geocoders import Nominatim
 import json
 from pprint import pprint
+from string import digits
 
 all_lats = []
 all_longs = []
-list_of_crimes = []
 apartments = []
 apartments_out = []
 outfile = open('apartment_summary_vectors_with_crimes_and_permits.json', 'w+')
 
 with open('building_permit_cleaned_up.json') as building_permits:
     permit_data = json.load(building_permits)
-    with open('apartment_summary_vectors.json') as apartment_data1:
+    with open('apartment_summary_vectors_with_crimes.json') as apartment_data1:
         apartment_data = json.load(apartment_data1)
         for apartment in apartment_data:
             apartment.update({"permits":[]})
             apartments.append(apartment)
 
         for permit in permit_data:
+            latit =  (permit['lats'])
+            print latit[0]
 
-            permit_lat_long = tuple((permit['lats'], permit['longs']))
-            print permit_lat_long
+          #  latter = latit[0:len(latit)]
+            #print latter
+            longit =  (permit['longs'])
+           # longer = longit[0:len(longit)]
+            #print longer
 
-    #        for i, apartment in enumerate(apartment_data):
-    #            apartment_long_lat = (apartment['lats'],apartment['longs'])
-    #            if (vincenty(permit_lat_long, apartment_long_lat).miles) <= .25:
-    #                apartments[i]["permits"].append(permit)
+            permit_lat_long = tuple((latit[0], longit[0]))
+
+            for i, apartment in enumerate(apartment_data):
+                apartment_long_lat = (apartment['lats'],apartment['longs'])
+                if (vincenty(permit_lat_long, apartment_long_lat).miles) <= .20:
+                    apartments[i]["permits"].append(permit)
 
 for a in apartments:
-    json.dump({'name': a["name"], 'address': a["address"], 'lats': a["lats"], 'longs': a["longs"], 'crimes': a["crimes"]}, outfile)
+    json.dump({'name': a["name"], 'address': a["address"], 'lats': a["lats"], 'longs': a["longs"], 'crimes': a["crimes"],'permits': a["permits"]}, outfile)
     outfile.write(",")
--- building_permit_json.py
+++ building_permit_json.py
@@ -15,13 +15,13 @@ outfile = open('building_permit_clean.json','w')
 
     #print all_status
 with open('building_permit_filtered.txt') as infile2:
-    all_address = [line.strip('\"').split(',')[15:16] for line in infile2.readlines()]
+    all_address = [line.strip("\\\"").split(',')[15:16] for line in infile2.readlines()]
 with open('building_permit_filtered.txt') as infile2:
-    all_status= [line.strip('\"').split(',')[14:15] for line in infile2.readlines()]
+    all_status= [line.strip("\\\"").split(',')[14:15] for line in infile2.readlines()]
 with open('building_permit_filtered.txt') as lats:
-    all_lats = [line.strip('\"').split(',')[55:56] for line in lats.readlines()]
+    all_lats = [line.strip("\\\"").split(',')[55:56] for line in lats.readlines()]
 with open('building_permit_filtered.txt') as longs:
-    all_longs = [line.strip('\"').split(',')[56:57] for line in longs.readlines()]
+    all_longs = [line.strip("\\\"").split(',')[56:57] for line in longs.readlines()]
 
 outfile.write("[")
 
